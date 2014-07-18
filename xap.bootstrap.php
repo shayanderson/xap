78e8b00c515e31b496c591c95bde7254f401675f
Shay Anderson <nexframe@yahoo.com>
2014-07-18 09:28:36 -0400

    mod auto load Model, Decorator

--- xap.bootstrap.php
+++ xap.bootstrap.php
@@ -7,10 +7,10 @@
 require_once './lib/Xap/Engine.php';
 
 // import Xap Model class (if using '/model' query option)
-// require_once './lib/Xap/Model.php';
+require_once './lib/Xap/Model.php';
 
 // import Xap Decorator class (if using decorators)
-// require_once './lib/Xap/Decorator.php';
+require_once './lib/Xap/Decorator.php';
 
 // import xap() function
 require_once './lib/Xap/xap.php';
